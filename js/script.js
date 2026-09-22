0ef903256ba14fe8b22435e1b8bb1582d580c342
paula <paulaaf.2003@gmail.com>
2026-09-22 19:26:47 +0200

    JS

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// alert("Online!")
+
+// console.log("Hola consola!")
+
+// VARIABLES Y TIPOS DE DATOS
+
+var username = "Adrián"; // texto o string 
+
+var uage = 37; // numero entero - integer
+
+var ulicense = true; // boolean - true o false
+
+var colors = ["red", "blue", "green"] // lista o array
+
+var values = [21, 45, 76, 23]
+
+// llamar a una variable o invocar 
+
+username = "Pepito"
+
+console.log(username);
+
+var total_price;
+
+var price01 = 23;
+var price02 = 42;
+
+total_price = price01 + price02;
+
+console.log(total_price)
